c3038771daef13ed8af1bddcf052748d1e01cffd
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-12-12 23:05:05 -0300

    feat: updating tcp connection

--- src/services/centralServer.py
+++ src/services/centralServer.py
@@ -1,7 +1,8 @@
 from socket import *
+import os
 
 default_host = gethostname()
-default_port = 55552
+default_port = 55562 
 default_message = ''
 
 central_server_host = default_host
@@ -15,10 +16,19 @@ print(f'Servidor Central conectado no HOST:  {central_server_host} e PORTA: {cen
 distributed_server_connection, distributed_server_address = central_server.accept()
 print(f'O Servidor distribuido: {distributed_server_address} se conectou')
 
-# Recebimento de mensagens
 while 1:
+    print('=================================================')
+    print('============= FSE - TRABALHO 01 =================')
+    print('=================================================')
+    new_message = input('1 - VER DISPOSITIVOS\n2 - ATUALIZAR ESTADOS\n')
+    distributed_server_connection.send(new_message.encode());
+
     message_received = distributed_server_connection.recv(1024)
-    if default_message != message_received:
-        print(message_received.decode())
+    print(message_received.decode())
+
+    clear_page = int(input('LIMPAR TELA (1)\n'))
+    if clear_page == 1:
+        os.system('clear')
+
 
 distributed_server_connection.close()
