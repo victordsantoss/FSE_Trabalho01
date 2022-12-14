83de4c573ce93ef073952776ed2d698a1248ef11
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-12-14 12:18:47 -0300

    feat: adding thread system in received messages

--- src/services/centralServer.py
+++ src/services/centralServer.py
@@ -1,5 +1,6 @@
 from socket import *
 import os
+import threading
 
 default_host = gethostname()
 default_port = 55562 
@@ -18,33 +19,39 @@ print(f'Servidor Central conectado no HOST:  {central_server_host} e PORTA: {cen
 distributed_server_connection, distributed_server_address = central_server.accept()
 print(f'O Servidor distribuido: {distributed_server_address} se conectou')
 
-while 1:
-    print('=================================================')
-    print('============= FSE - TRABALHO 01 =================')
-    print('=================================================')
-    commands = ''
-    new_message = input('VER DISPOSITIVOS DE ENTRADA (1)\nVER DISPOSITIVOS DE SAÍDA (2)\nVER VALORES DE TEMPERATURA E UMIDADE (3)\n ACIONAR DISPOSITIVOS (4)\n')
-    # OPCOES DE LEITURA
-    if int(new_message) != 4:
-        commands = f'1,{new_message}'
-        distributed_server_connection.send(commands.encode());
-    # OPÇOES DE ESCRITA/MUDANÇA DE STADO
-    if int(new_message) == 4:
-        os.system('clear')
-        print('=================================================')
-        print('======== SELECIONE OS DISPOSITIVOS ==============')
-        print('=================================================')
-        new_message = input('L_01 (18)\nL_02 (23)\nAC (24)\n PR (25)\n')
-        commands = f'2,{new_message}'
-        distributed_server_connection.send(commands.encode());
-
-
+def handleReceivedMessages(distributed_server_connection): 
     message_received = distributed_server_connection.recv(1024)
     print(message_received.decode())
 
-    clear_page = int(input('LIMPAR TELA (1)\n'))
-    if clear_page == 1:
-        os.system('clear')
+def main():
+    while 1:
+        print('=================================================')
+        print('============= FSE - TRABALHO 01 =================')
+        print('=================================================')
+        commands = ''
+        new_message = input('VER DISPOSITIVOS DE ENTRADA (1)\nVER DISPOSITIVOS DE SAÍDA (2)\nVER VALORES DE TEMPERATURA E UMIDADE (3)\nACIONAR DISPOSITIVOS (4)\n')
+        # OPCOES DE LEITURA
+        if int(new_message) != 4:
+            commands = f'1,{new_message}'
+        # OPÇOES DE ESCRITA/MUDANÇA DE STADO
+        if int(new_message) == 4:
+            os.system('clear')
+            print('=================================================')
+            print('======== SELECIONE OS DISPOSITIVOS ==============')
+            print('=================================================')
+            new_message = input('L_01 (18)\nL_02 (23)\nAC (24)\nPR (25)\n')
+            commands = f'2,{new_message}'
+
+        distributed_server_connection.send(commands.encode())
+        thread = threading.Thread(target=handleReceivedMessages, args=(distributed_server_connection,))
+        thread.start()
+
+        clear_page = int(input('LIMPAR TELA (1)\n'))
+        if clear_page == 1:
+            os.system('clear')
 
 
-distributed_server_connection.close()
+    distributed_server_connection.close()
+
+if __name__ == "__main__":
+    main()
