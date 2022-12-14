8f5a15e81f1387b67a969286870135e4ebe16f18
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-12-14 12:19:16 -0300

    feat: adding thread system in send messages

--- src/services/distributedServer.py
+++ src/services/distributedServer.py
@@ -2,6 +2,7 @@ from socket import *
 import RPi.GPIO as GPIO
 import adafruit_dht
 import time
+import threading
 
 # SERVER CONFIGS
 default_host = gethostname()
@@ -48,7 +49,7 @@ def handleOutputDevices():
     AC = "ON" if GPIO.input(devices["AC"]) == 1 else "OF"
     PR = "ON" if GPIO.input(devices["PR"]) == 1 else "OF"
     AL_BZ = "ON" if GPIO.input(devices["AL_BZ"]) == 1 else "OF"
-    return 'L01:'  + L_01 + '\n' + 'L_02: '  + L_02 +  '\n' + 'AC: '  + AC + '\n' + 'PR: ' + PR + '\n' + 'AL_BZ: ' + AL_BZ +'\n'
+    return '\nLEITURA DE DISPOSITIVOS DE SAÍDA: \n' + 'L01:'  + L_01 + '\n' + 'L_02: '  + L_02 +  '\n' + 'AC: '  + AC + '\n' + 'PR: ' + PR + '\n' + 'AL_BZ: ' + AL_BZ +'\n'
 
 def handleInputDevices():
     SPres =  "ON" if GPIO.input(devices["SPres"]) == 1 else "OF"
@@ -57,7 +58,7 @@ def handleInputDevices():
     SPor = "ON" if GPIO.input(devices["SPor"]) == 1 else "OF"
     SC_IN = "ON" if GPIO.input(devices["SC_IN"]) == 1 else "OF"
     SC_OUT = "ON" if GPIO.input(devices["SC_OUT"]) == 1 else "OF"
-    return 'SPres:'  + SPres + '\n' + 'SFum: '  + SFum +  '\n' + 'SJan: '  + SJan + '\n' + 'SPor: ' + SPor + '\n' + 'SC_IN: ' + SC_IN + '\n' + 'SC_OUT: ' + SC_OUT + '\n'
+    return '\nLEITURA DE DISPOSITIVOS DE ENTRADA: \n' + 'SPres:'  + SPres + '\n' + 'SFum: '  + SFum +  '\n' + 'SJan: '  + SJan + '\n' + 'SPor: ' + SPor + '\n' + 'SC_IN: ' + SC_IN + '\n' + 'SC_OUT: ' + SC_OUT + '\n'
 
 def handleTemperature():
     dhtDevice = adafruit_dht.DHT22(devices["DHT22"])
@@ -67,6 +68,7 @@ def handleTemperature():
             temperature_f = temperature_c * (9 / 5) + 32
             humidity = dhtDevice.humidity
             print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(temperature_f, temperature_c, humidity))
+            return "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(temperature_f, temperature_c, humidity)
         except RuntimeError as error:
             print(error.args[0])
             time.sleep(2.0)
@@ -79,8 +81,12 @@ def handleTemperature():
 def handleUpdateDeviceState(pin_number):
     print(pin_number, type(pin_number))
     GPIO.output(pin_number, GPIO.LOW) if GPIO.input(pin_number) == 1 else GPIO.output(pin_number, GPIO.HIGH)
-    return handleOutputDevices()
+    res = handleOutputDevices()
+    return res
    
+def handleSendMessages(message_send):
+    distributed_server.send(message_send.encode())
+
 def main():
     handleGPIOConfig()
     message_send = ''
@@ -93,16 +99,15 @@ def main():
         if int(message_received[0]) == 1:  
             if int(message_received[1]) == 1:
                 message_send = handleOutputDevices()
-                distributed_server.send(message_send.encode())
             elif int(message_received[1]) == 2:
                 message_send = handleInputDevices()
-                distributed_server.send(message_send.encode())
             elif int(message_received[1]) == 3:
                 message_send = handleTemperature()
-                distributed_server.send(message_send.encode())
         if int(message_received[0]) == 2:
             message_send = handleUpdateDeviceState(int(message_received[1]))
             distributed_server.send(message_send.encode())
+        send_thread = threading.Thread(target=handleSendMessages, args=(message_send, ))
+        send_thread.start()
 
 if __name__ == "__main__":
     main()
