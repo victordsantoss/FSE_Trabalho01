e8d5b769e99be40f50833522b56ca9695d0e1c63
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-12-19 20:07:06 -0300

    feat: adding class select, view count people and alarms

--- src/services/distributedServer.py
+++ src/services/distributedServer.py
@@ -1,5 +1,4 @@
 from socket import *
-from markupsafe import string
 import RPi.GPIO as GPIO
 import adafruit_dht
 import time
@@ -8,14 +7,23 @@ import json
 import board
 
 # JSON CONFIGS
-with open("../utils/configuracao_sala_01.json", encoding='utf-8') as meu_json:
-    devices = json.load(meu_json)
+sala = int(input("DESEJA CONECTAR EM QUAL SALA? (DEVE SER A MESMA ESCOLHIDA PELO SERVIDOR CENTRAL)\nSALA 01 (1)\nSALA 02 (2): "))
+global devices
+if sala == 1: 
+    print('ENTREI SALA 01')
+    with open("../utils/configuracao_sala_01.json", encoding='utf-8') as meu_json: devices = json.load(meu_json)
+if sala == 2:
+    print('ENTREI SALA 01')
+    with open("../utils/configuracao_sala_02.json", encoding='utf-8') as meu_json: devices = json.load(meu_json)
 
 # SERVER CONFIGS
 default_host = devices['ip_servidor_central']
 default_port = devices['porta_servidor_central'] 
 distributed_server = socket(AF_INET, SOCK_STREAM)
 distributed_server.connect((default_host, default_port))
+
+global count
+countP = 0
 
 # GPIO CONFIGS
 def handleGPIOConfig():
@@ -28,15 +36,17 @@ def handleGPIOConfig():
     GPIO.setup(devices["outputs"][3]["gpio"], GPIO.OUT)  
     GPIO.setup(devices["outputs"][4]["gpio"], GPIO.OUT) 
     # DISPOSITIVOS DE ENTRADA
-    GPIO.setup(devices["inputs"][0]["gpio"], GPIO.OUT) 
-    GPIO.setup(devices["inputs"][1]["gpio"], GPIO.OUT) 
-    GPIO.setup(devices["inputs"][2]["gpio"], GPIO.OUT)
-    GPIO.setup(devices["inputs"][3]["gpio"], GPIO.OUT) 
-    GPIO.setup(devices["inputs"][4]["gpio"], GPIO.OUT) 
-    GPIO.setup(devices["inputs"][5]["gpio"], GPIO.OUT) 
+    GPIO.setup(devices["inputs"][0]["gpio"], GPIO.IN) 
+    GPIO.setup(devices["inputs"][1]["gpio"], GPIO.IN) 
+    GPIO.setup(devices["inputs"][2]["gpio"], GPIO.IN)
+    GPIO.setup(devices["inputs"][3]["gpio"], GPIO.IN) 
+    GPIO.setup(devices["inputs"][4]["gpio"], GPIO.IN) 
+    GPIO.setup(devices["inputs"][5]["gpio"], GPIO.IN) 
 
 
 def handleOutputDevices():
+    if (GPIO.input(devices["inputs"][0]["gpio"]) == 1) or (GPIO.input(devices["inputs"][2]["gpio"]) == 1):  GPIO.output(devices["outputs"][4]["gpio"], GPIO.HIGH) 
+    else: GPIO.output(devices["outputs"][4]["gpio"], GPIO.LOW) 
     res = [
         {
             "type": "lampada",
@@ -49,7 +59,6 @@ def handleOutputDevices():
             "tag": "Lâmpada 02",
             "gpio": 23,
             "state": "ON" if GPIO.input(devices["outputs"][1]["gpio"]) == 1 else "OF"
-
         },
         {
             "type": "projetor",
@@ -67,7 +76,7 @@ def handleOutputDevices():
             "type": "alarme",
             "tag": "Sirene do Alarme",
             "gpio": 8,
-            "state": "ON" if GPIO.input(devices["outputs"][4]["gpio"]) == 1 else "OF"
+            "state": "ALERTA ALARME ESTÁ ACIONADO!!" if GPIO.input(devices["outputs"][4]["gpio"]) == 1 else "OF"
         }
     ]
     res = json.dumps(res)
@@ -116,7 +125,7 @@ def handleInputDevices():
     return res
 
 def handleTemperature():
-    dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False) # "DHT22": 4
+    dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False) # "DHT22": 4
     try:
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
@@ -160,57 +169,87 @@ def handleUpdateAllDevices(type):
             print(f'Dispositivo: {x["tag"]} | ESTÁ LIGADO')
             GPIO.output(x["gpio"], GPIO.LOW) 
         print("ON" if GPIO.input(devices["inputs"][5]["gpio"]) == 1 else "OF")
-        time.sleep(2.0)
+        time.sleep(0.5)
 
     return handleOutputDevices()
 
-def handleSendMessages(message_send):
-    distributed_server.sendall(message_send.encode())
+def handleCountPeople():
+    GPIO.setup(devices["inputs"][4]["gpio"], GPIO.IN) 
+    GPIO.setup(devices["inputs"][5]["gpio"], GPIO.IN) 
+    GPIO.add_event_detect(devices['inputs'][4]['gpio'], GPIO.RISING)
+    GPIO.add_event_detect(devices['inputs'][5]['gpio'], GPIO.RISING)
+    try:
+        while 1:
+            time.sleep(0.0001)
+            if GPIO.event_detected(devices["inputs"][4]["gpio"]):
+                countP = countP + 1
+                print("Entrou +1", countP)
+            if GPIO.event_detected(devices["inputs"][5]["gpio"]):
+                countP = countP - 1
+                if countP < 0:
+                    countP = 0
+            print("Saiu +1", countP)
+            print("Número de Pessoas atual na sala: ", countP)
+    except:
+        print('Error counting people')
 
 def main():
     handleGPIOConfig()
     while 1:
         message = distributed_server.recv(1024)
-        print(message.decode())
         message = message.decode()
         message_received = message.split(',')
-
+        print("message",message_received)
         if int(message_received[0]) == 1:  
             if int(message_received[1]) == 1:
                 message_send = handleOutputDevices()
-                threading.Thread(target=handleSendMessages, args=(message_send, )).start()
+                distributed_server.send(message_send.encode())
             elif int(message_received[1]) == 2:
                 message_send = handleInputDevices()
-                threading.Thread(target=handleSendMessages, args=(message_send, )).start()
+                distributed_server.send(message_send.encode())
             elif int(message_received[1]) == 3:
                     message_send = handleTemperature()
                     if message_send is not None:
                         distributed_server.sendall(message_send.encode())
-                  
+            elif int(message_received[1]) == 7:
+                res = [
+                    {
+                        "tag": 'Sensor de Contagem de Pessoas',
+                        "state": f'Número de Pessoas na sala: {countP}'
+                    }
+                ]
+                res = json.dumps(res)
+                distributed_server.send(res.encode())  
+
         if int(message_received[0]) == 2:
             message_send = handleUpdateDeviceState(int(message_received[1]), message_received[2])
-            threading.Thread(target=handleSendMessages, args=(message_send, )).start()
+            distributed_server.send(message_send.encode())
 
         if int(message_received[0]) == 3:
             if int(message_received[1]) == 1:
                 message_send = handleUpdateAllDevices(1)
-                threading.Thread(target=handleSendMessages, args=(message_send, )).start()
+                distributed_server.send(message_send.encode())
             if int(message_received[1]) == 2:
                 message_send = handleUpdateAllDevices(2)
-                threading.Thread(target=handleSendMessages, args=(message_send, )).start()
+                distributed_server.send(message_send.encode())
 
+def handleAlarms():
+    GPIO.setup(devices["inputs"][0]["gpio"], GPIO.IN) 
+    GPIO.setup(devices["inputs"][2]["gpio"], GPIO.IN) 
+    GPIO.setup(devices["outputs"][4]["gpio"], GPIO.OUT) 
+    while 1:
+        statePres = "ON" if GPIO.input(devices["inputs"][0]["gpio"]) == 1 else "OF"
+        stateFum = "ON" if GPIO.input(devices["inputs"][2]["gpio"]) == 1 else "OF"
+        print(f'{devices["inputs"][0]["tag"]}: {statePres}, {devices["inputs"][2]["tag"]}: {stateFum}')
+        if statePres == "ON":
+            print(f'ALERTA! O SENSOR {devices["inputs"][0]["gpio"]} LIGADO - O ALARME SERÁ ATIVADO')
+            GPIO.output(devices["outputs"][4]["gpio"], GPIO.HIGH) 
+        if stateFum == "ON":
+            print(f'ALERTA! O SENSOR {devices["inputs"][2]["gpio"]} LIGADO - O ALARME SERÁ ATIVADO')
+        time.sleep(5.0)
 
-# def handleAlarms():
-#     GPIO.setup(devices["inputs"][1]["gpio"], GPIO.OUT) 
-#     while 1:
-#         state = "ON" if GPIO.input(devices["inputs"][1]["gpio"]) == 1 else "OF"
-#         print("state", state, GPIO.input(devices["inputs"][1]["gpio"]))
-#         if state == "ON":
-#             print("TÁ ONLINE PORRA")
-#             distributed_server.sendall(state.encode())
-#         time.sleep(2.0)
-
-# threading.Thread(target=handleAlarms).start()
+threading.Thread(target=handleAlarms,).start()
+threading.Thread(target=handleCountPeople,).start()
 
 if __name__ == "__main__":
     main()
